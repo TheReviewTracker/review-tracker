a72d9094dc73cab9f76d506f492d5660fa7dd63c
haneul-hcg <hnleehcg@gmail.com>
2023-09-03 23:11:50 +0900

    FEAT: MUI 설치 및 로그인 화면 마무리 작업

--- src/app/sign_in/page.tsx
+++ src/app/sign_in/page.tsx
@@ -1,11 +1,11 @@
-// 아래 라인 프로젝트 전체에 적용 필요
-/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+import Image from "next/image";
 import Cookies from "js-cookie";
+
+import { Box, Button, CircularProgress } from "@mui/material";
 
 import { CLIENT_ID } from "app/lib/env";
 
@@ -16,6 +16,7 @@ interface IProps {
 const SignInPage = ({ searchParams }: IProps) => {
   const { code } = searchParams;
   const router = useRouter();
+  const [authLoading, setAuthLoading] = useState(false);
 
   if (Cookies.get("token")) {
     Cookies.remove("token");
@@ -24,6 +25,8 @@ const SignInPage = ({ searchParams }: IProps) => {
   useEffect(() => {
     if (code) {
       const getToken = async () => {
+        setAuthLoading(true);
+
         const res = await fetch(`http://localhost:9850/api/auth?code=${code}`, {
           method: "post",
         });
@@ -38,13 +41,59 @@ const SignInPage = ({ searchParams }: IProps) => {
   }, []);
 
   return (
-    <div>
-      <Link
-        href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${"http://localhost:9850/sign_in"}`}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        background: "#FAFAFA",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: 800,
+          height: 165,
+          border: "1px solid",
+          background: "#FFF",
+          borderRadius: "8px",
+          borderColor: "#EEE",
+        }}
       >
-        Github Auth
-      </Link>
-    </div>
+        <div style={{ fontWeight: "bold", fontSize: 26, marginTop: 20 }}>
+          Review Tracker
+        </div>
+        {!authLoading ? (
+          <Button
+            href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${"http://localhost:9850/sign_in"}`}
+            sx={{
+              mt: 6,
+              color: "white",
+              textTransform: "none",
+              backgroundColor: "black",
+              "&:hover": {
+                backgroundColor: "black",
+              },
+            }}
+          >
+            <Image
+              src="/github-mark-white.svg"
+              alt="github-mark"
+              width={25}
+              height={25}
+              style={{ marginRight: 10 }}
+            />
+            <div>Log in with GitHub</div>
+          </Button>
+        ) : (
+          <CircularProgress sx={{ mt: 6 }} />
+        )}
+      </Box>
+    </Box>
   );
 };
 
